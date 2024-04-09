d88b230ce6851ac4ca5f71a7559a2d3eef055205
Salimans <Maico.Salimans@dnv.com>
2024-04-09 11:19:47 +0200

    les 5.6

--- voetbal/blog/models.py
+++ voetbal/blog/models.py
@@ -1,19 +1,21 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+from django.core.management.base import BaseCommand
 
 
 class Voetbalspelers(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    voetballer = models.CharField(max_length=200)
-    huidige_club = models.CharField(max_length=200)
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    naam_auteur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    naam_voetballer = models.CharField(max_length=200)
+    actuele_club = models.CharField(max_length=200)
+    datum_invoer = models.DateTimeField(default=timezone.now)
+    datum_laatste_aanpassing = models.DateTimeField(auto_now=True)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.naam_voetballer
+
+
